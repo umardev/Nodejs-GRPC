f6c56fe3c5168e8ec67f7dfc3e6f7c79c6beba22
umar khan <umar@techcetra.com>
2020-09-14 12:02:34 +0530

    Config

--- config/constants.js
+++ config/constants.js
@@ -1,13 +1,13 @@
 var constants = {
-    DB_DATABASE: "sales",
-    DB_HOST: "salespersonng.cfgzkreakpnz.ap-south-1.rds.amazonaws.com",
+    DB_DATABASE: "",
+    DB_HOST: "",
     // DB_HOST: "wecare-production.cpwtzppammuy.us-east-1.rds.amazonaws.com",
-    DB_USER: "salespersonng",
-    DB_PASSWORD: "SzBeH9bnMMYJRWAQvLRy",
+    DB_USER: "",
+    DB_PASSWORD: "",
 
-    DB_HOST2: "wecare-staging.cesogfqxva5x.ap-south-1.rds.amazonaws.com",
-    DB_USER2: "wecare",
-    DB_PASSWORD2: "kdym|h%YOlkM,8GMRoQ~;6?}qD5bZcZ1",
-    DB_DATABASE2: "eluminarie",
+    DB_HOST2: "",
+    DB_USER2: "",
+    DB_PASSWORD2: "",
+    DB_DATABASE2: "",
 }
 module.exports = constants;
